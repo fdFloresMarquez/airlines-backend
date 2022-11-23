7ff0d1dd245d097cc292a14be4dbfbe3c1920d1f
Facundo <floresfacundo68@gmail.com>
2022-11-23 19:45:41 -0300

    add airlines controllers

--- src/controllers/airlines.controllers.ts
+++ src/controllers/airlines.controllers.ts
@@ -4,6 +4,7 @@ import { Airline } from "../entities/Airline";
 export const createAirline = async (req: Request, res: Response) => {
   try {
     const { iata_code, airline } = req.body;
+
     const airlineEntity = new Airline();
 
     airlineEntity.iata_code = iata_code;
@@ -21,7 +22,11 @@ export const createAirline = async (req: Request, res: Response) => {
 
 export const getAirlines = async (req: Request, res: Response) => {
   try {
-    const airlines = await Airline.find();
+    const airlines = await Airline.find({
+      relations: {
+        flights: true,
+      },
+    });
 
     return res.json(airlines);
   } catch (error) {
@@ -40,11 +45,51 @@ export const updateAirline = async (req: Request, res: Response) => {
     });
 
     if (!airlineEntity)
-      return res.status(404).json({ message: "User does not exist" });
+      return res.status(404).json({ message: "Airline does not exist" });
 
     await Airline.update({ id: parseInt(id) }, req.body);
 
     return res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+};
+
+export const deleteAirline = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const airlineEntity = await Airline.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if (!airlineEntity)
+      return res.status(404).json({ message: "Airline does not exist" });
+
+    await Airline.delete({ id: parseInt(id) });
+
+    return res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+};
+
+export const getAirlineById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const airlineEntity = await Airline.findOneBy({
+      id: parseInt(id),
+    });
+
+    if (!airlineEntity)
+      return res.status(404).json({ message: "Airline does not exist" });
+
+    return res.json(airlineEntity);
   } catch (error) {
     if (error instanceof Error) {
       return res.status(500).json({ message: error.message });

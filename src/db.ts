fc38cef44bd8c3f984d7293159429273c24d4131
Facundo <floresfacundo68@gmail.com>
2022-11-28 09:00:46 -0300

    chane in .env variables

--- src/db.ts
+++ src/db.ts
@@ -5,13 +5,14 @@ import { Airport } from "./entities/Airport";
 import { Flight } from "./entities/Flight";
 
 dotenv.config();
+const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
 // Create database with respective
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  username: "postgres",
+  username: dbUsername,
   password: dbPassword,
   port: 5432,
   database: "airlinesdb",
